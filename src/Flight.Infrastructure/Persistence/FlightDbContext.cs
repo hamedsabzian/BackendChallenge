592bf4f2c09d1dcbb06e8048b0e1c973f2fd6c79
Hamed Sabzian <hamed.sabzian.87@gmail.com>
2024-09-05 10:27:44 +0330

    Add DbContext and implement the repository

---
+++ src/Flight.Infrastructure/Persistence/FlightDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using Flight.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flight.Infrastructure.Persistence;
+
+public class FlightDbContext : DbContext
+{
+    public FlightDbContext()
+    {
+    }
+
+    public FlightDbContext(DbContextOptions<FlightDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Route> Routes { get; set; }
+    public DbSet<Domain.Entities.Flight> Flights { get; set; }
+    public DbSet<Subscription> Subscriptions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite("Data Source=migrations-target.db;");
+        }
+    }
+}
---
+++ src/Flight.Infrastructure/Persistence/Repositories/FlightRepository.cs
@@ -0,0 +1,20 @@
+﻿using Flight.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flight.Infrastructure.Persistence.Repositories;
+
+public class FlightRepository(FlightDbContext dbContext) : IFlightRepository
+{
+    public Task<List<Domain.Entities.Flight>> GetInterestedAgencyFlights(int agencyId, DateTime startDate, DateTime endDate)
+    {
+        var query = from subscription in dbContext.Subscriptions
+            join route in dbContext.Routes
+                on new { subscription.OriginCityId, subscription.DestinationCityId } equals new { route.OriginCityId, route.DestinationCityId }
+            join flight in dbContext.Flights
+                on route.Id equals flight.RouteId
+            where subscription.AgencyId == agencyId && startDate <= flight.DepartureTime && flight.DepartureTime <= endDate
+            select flight;
+
+        return query.Include(f => f.Route).AsNoTracking().ToListAsync();
+    }
+}
