c04d44abdf0ae731e9d78eca763719a2745fe4db
Hamed Sabzian <hamed.sabzian.87@gmail.com>
2024-09-05 10:30:21 +0330

    Register dependencies

---
+++ src/Flight.Application/DependencyExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Flight.Application.Queries;
+using Flight.Application.Queries.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Flight.Application;
+
+public static class DependencyExtensions
+{
+    public static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+    {
+        services.AddScoped<FlightService>();
+        services.AddScoped<FlightServiceMetricProxy>();
+        services.AddScoped<IFlightService, FlightServiceStoreResultDecorator>();
+
+        return services;
+    }
+}
---
+++ src/Flight.Infrastructure/DependencyExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Flight.Application.Options;
+using Flight.Application.Queries.Interfaces;
+using Flight.Domain.Interfaces;
+using Flight.Infrastructure.FileSystem;
+using Flight.Infrastructure.Persistence;
+using Flight.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Flight.Infrastructure;
+
+public static class DependencyExtensions
+{
+    public static IServiceCollection AddInfrastructureDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<FlightOptions>(configuration.GetSection("Flight"));
+
+        services.AddDbContext<FlightDbContext>(builder =>
+        {
+            builder.UseSqlite(configuration.GetConnectionString("Sqlite"));
+        });
+        services.AddScoped<IFlightRepository, FlightRepository>();
+        services.AddScoped<IFlightChangeDetectionResultStorage, FlightChangeDetectionResultFileStorage>();
+        return services;
+    }
+}
