200e6181266c11669c7ff5acad4699473eb6b988
Hamed Sabzian <hamed.sabzian.87@gmail.com>
2024-09-05 10:30:59 +0330

    Complete CLI project

--- src/Flight.Cli/Program.cs
+++ src/Flight.Cli/Program.cs
@@ -1,3 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Flight.Application;
+using Flight.Application.Queries.DetectFlightChanges;
+using Flight.Application.Queries.Interfaces;
+using Flight.Infrastructure;
+using Flight.Infrastructure.Persistence;
+using Flight.Infrastructure.Persistence.Seeders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-Console.WriteLine("Hello, World!");
+if (args.Length != 3 ||
+    !DateTime.TryParse(args[0], out var startTime) ||
+    !DateTime.TryParse(args[1], out var endTime) ||
+    !int.TryParse(args[2], out var agencyId))
+{
+    Console.WriteLine("The correct parameters (Start Date, End Date, Agency ID) is not provided.");
+    return 1;
+}
+
+var configuration = ProvideConfiguration();
+
+var services = new ServiceCollection()
+    .AddSingleton(configuration)
+    .AddLogging(loggingOptions =>
+    {
+        loggingOptions.AddConfiguration(configuration.GetSection("Logging"))
+            .AddSimpleConsole(options =>
+            {
+                options.TimestampFormat = "[HH:mm:ss] ";
+            });
+    })
+    .AddApplicationDependencies()
+    .AddInfrastructureDependencies(configuration);
+
+var serviceProvider = services.BuildServiceProvider();
+
+var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+await InitialDatabase();
+
+using var dependencyScope = serviceProvider.CreateScope();
+var flightService = dependencyScope.ServiceProvider.GetRequiredService<IFlightService>();
+await flightService.DetectChangesForAgency(new DetectFlightChangesQuery(agencyId, startTime, endTime));
+
+return 0;
+
+IConfigurationRoot ProvideConfiguration()
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false);
+    return builder.Build();
+}
+
+async ValueTask InitialDatabase()
+{
+    using var migrationScope = serviceProvider.CreateScope();
+    var dbContext = migrationScope.ServiceProvider.GetRequiredService<FlightDbContext>();
+    dbContext.Database.Migrate();
+
+    if (!dbContext.Subscriptions.Any())
+    {
+        var rootPath = configuration["InitialDataPath"]!;
+        await new SubscriptionCsvSeeder().Seed(Path.Combine(rootPath, "subscriptions.csv"), dbContext);
+        logger.LogInformation("The subscriptions are imported successfully");
+
+        await new RouteCsvSeeder().Seed(Path.Combine(rootPath, "routes.csv"), dbContext);
+        logger.LogInformation("The routes are imported successfully");
+
+        await new FlightCsvSeeder().Seed(Path.Combine(rootPath, "flights.csv"), dbContext);
+        logger.LogInformation("The flights are imported successfully");
+    }
+    else
+    {
+        logger.LogInformation("Data has already been imported");
+    }
+}
